008e45d00e817a449f50e2fbfee6adc66589635d
MaeinesenieaM <david.costa.gomes71@gmail.com>
2026-07-21 23:57:04 -0300

    feat: teste de calendario.

--- scripts/index.mjs
+++ scripts/index.mjs
@@ -13,6 +13,64 @@ var require_index = __commonJS({
     var g = game;
     console.log(g.dnd5e);
     console.log("FUNCIONA!");
+    var CALENDARIO_NOME_PERDIDO_CONFIG = {
+      name: "calendario maine",
+      description: "muito foda",
+      years: {
+        yearZero: 0,
+        firstWeekday: 0,
+        leapYear: {
+          leapInterval: 4,
+          leapStart: 8
+        }
+      },
+      months: {
+        name: "Eita",
+        values: [
+          {
+            name: "Fodas",
+            abbreviation: "foda",
+            ordinal: 1,
+            days: 100
+          },
+          {
+            name: "Loucos",
+            abbreviation: "louco",
+            ordinal: 2,
+            days: 100
+          }
+        ]
+      },
+      days: {
+        daysPerYear: 200,
+        hoursPerDay: 24,
+        minutesPerDay: 60,
+        secondsPerMinute: 60,
+        values: [
+          { name: "um", ordinal: 1 },
+          { name: "dois", ordinal: 2 },
+          { name: "quarta", ordinal: 3 },
+          { name: "tres", ordinal: 4 },
+          { name: "cinco", ordinal: 5 }
+        ]
+      },
+      seasons: {
+        values: [
+          {
+            monthStart: 1,
+            monthEnd: 2,
+            name: "INFERNO"
+          }
+        ]
+      }
+    };
+    Hooks.once("init", function() {
+      CONFIG.DND5E.calendar.calendars.push({
+        value: "teste-maine",
+        label: "MAINE TESTE",
+        config: CALENDARIO_NOME_PERDIDO_CONFIG
+      });
+    });
   }
 });
 export default require_index();
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,63 @@ const g = game as ReadyGame;
 
 console.log(g.dnd5e);
 console.log("FUNCIONA!");
+
+const CALENDARIO_NOME_PERDIDO_CONFIG = {
+	name: "calendario maine",
+	description: "muito foda",
+	years: {
+		yearZero: 0,
+		firstWeekday: 0,
+		leapYear: {
+			leapInterval: 4,
+			leapStart: 8
+		}
+	},
+	months: {
+		name: "Eita",
+		values: [
+			{
+				name: "Fodas",
+				abbreviation: "foda",
+				ordinal: 1,
+				days: 100
+			},
+			{
+				name: "Loucos",
+				abbreviation: "louco",
+				ordinal: 2,
+				days: 100
+			}
+		]
+	},
+	days: {
+		daysPerYear: 200,
+		hoursPerDay: 24,
+		minutesPerDay: 60,
+		secondsPerMinute: 60,
+		values: [
+			{ name: "um", ordinal: 1 },
+      		{ name: "dois", ordinal: 2 },
+      		{ name: "quarta", ordinal: 3 },
+      		{ name: "tres", ordinal: 4 },
+      		{ name: "cinco", ordinal: 5 },
+		]
+	},
+	seasons: {
+		values: [
+			{
+				monthStart: 1,
+				monthEnd: 2,
+				name: "INFERNO"
+			}
+		]
+	}
+}
+
+Hooks.once("init", function () {
+	CONFIG.DND5E.calendar.calendars.push({
+		value: "teste-maine",
+		label: "MAINE TESTE",
+		config: CALENDARIO_NOME_PERDIDO_CONFIG
+	});
+});
